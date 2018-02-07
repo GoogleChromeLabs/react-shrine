8995670aa72de6983d92cc8e07ba58cae29decd1
Addy Osmani <addyosmani@gmail.com>
2018-02-07 09:23:31 -0800

    Add initial store

---
+++ src/store.js
@@ -0,0 +1,46 @@
+/**
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { createStore, applyMiddleware, compose } from 'redux'
+import { routerMiddleware } from 'react-router-redux'
+import createHistory from 'history/createBrowserHistory'
+import rootReducer from './reducers'
+
+export const history = createHistory()
+
+const initialState = {}
+const enhancers = []
+const middleware = [
+  routerMiddleware(history)
+]
+
+if (process.env.NODE_ENV === 'development') {
+  const devToolsExtension = window.devToolsExtension
+
+  if (typeof devToolsExtension === 'function') {
+    enhancers.push(devToolsExtension())
+  }
+}
+
+const composedEnhancers = compose(
+  applyMiddleware(...middleware),
+  ...enhancers
+);
+
+const store = createStore(
+  rootReducer,
+  initialState,
+  composedEnhancers,
+);
+
+export default store;
