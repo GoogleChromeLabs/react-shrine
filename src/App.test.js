2f2ccaa688dbc5d272ba403b50572d5f1a889040
ri7nz <ri7nz.labs@gmail.com>
2019-01-09 23:30:01 +0700

    Fix Invariant Violation React-router in App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -11,11 +11,12 @@
  * limitations under the License.
  */
 
- import React from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {withRouter} from 'react-router-dom'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(withRouter(<App/>), div);
 });
