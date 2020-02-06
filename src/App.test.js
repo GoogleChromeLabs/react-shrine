26a6e4c1a01359c3fd52c732c6d8b043d169732a
Ricardo Aponte <rjaponte@rjaponte-macbookpro.roam.corp.google.com>
2020-02-06 14:49:18 -0500

    Getting tests to pass\

--- src/App.test.js
+++ src/App.test.js
@@ -17,8 +17,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+  <Router>
+    <App />
+    </Router>
+  , div);
 });
