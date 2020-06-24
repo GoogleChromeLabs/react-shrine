9e860fcc37943a943ed6c2cc7fb14d13273ab9f5
Ricardo Aponte <rjaponte@google.com>
2020-06-24 12:08:00 -0400

    Adding lighthouse steps to travis.ci

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'build')));
 
